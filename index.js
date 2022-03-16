364b87295c8ba7abc286bec81bf199ef88d85ab4
Sharvafork <89726230+Sharvafork@users.noreply.github.com>
2022-03-16 21:22:53 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,29 +1,34 @@
 const express = require('express');
-const res = require('express/lib/response');
 const app = express();
 const PORT = "8080";
 
-app.use( express.json() );
+app.use(express.json());
 
 app.post('/tshirt/:id', (req, res) => {
     const { id } = req.params;
-    const {logo} = req.body;
+    const { logo } = req.body;
 
     if (!logo) {
         res.status(418).send({ message: 'W need a logo!' });
     }
-        res.send({
-            tshirt: `${id} - ${logo}`,
-        })
+    res.send({
+        tshirt: `${id} - ${logo}`,
+    })
 })
+
+date = new Date();
 
 app.get('/tshirt', (req, res) => {
     res.status(200).send({
-        tshirt : '👕',
-        size : 'lol'
+        tshirt: date,
+        size: 'lol',
     })
 })
 
 
 
-res.sendFile(__dirname + '/index.html');
+app.listen(
+    PORT,
+    () => console.log(`Server is listening on port ${PORT}`)
+);
+
